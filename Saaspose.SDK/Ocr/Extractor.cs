3fcd1ce5ca85e844770059cceda0d7611ebe26da
imranra <imran.rafique@saaspose.com>
2012-07-13 09:26:32 -0700

    OCR overload methods

--- Saaspose.SDK/Ocr/Extractor.cs
+++ Saaspose.SDK/Ocr/Extractor.cs
@@ -11,6 +11,133 @@ namespace Saaspose.OCR
 {
     class Extractor
     {
+        public Extractor() { }
+        /// <summary>
+        /// Extract image text using default settings.
+        /// </summary>
+        /// <param name="fileName">The image file name.</param>
+        /// <param name="folder">The image file folder.</param>
+        /// <returns><see cref="OCRResponse"/> with the operation result.</returns>
+        public OCRResponse ExtractText(string imageFileName, string folder = null)
+        {
+            //build URI to extract text
+            string strURI = "";
+            if (folder == null || folder == string.Empty)
+                strURI = Product.BaseProductUri + "/ocr/" + imageFileName + "/recognize";
+            else
+                strURI = Product.BaseProductUri + "/ocr/" + imageFileName + "/recognize?folder=" + folder;
+
+            //sign URI
+            string signedURI = Utils.Sign(strURI);
+
+            //execute signed URI request and get response
+            Stream responseStream = Utils.ProcessCommand(signedURI, "GET");
+
+            StreamReader reader = new StreamReader(responseStream);
+            string strJSON = reader.ReadToEnd();
+
+            //Parse the json string to JObject
+            JObject parsedJSON = JObject.Parse(strJSON);
+
+            //Deserializes the JSON to a object. 
+            OCRResponse ocrResponse = JsonConvert.DeserializeObject<OCRResponse>(parsedJSON.ToString());
+
+            return ocrResponse;
+        }
+        /// <summary>
+        /// Extract image text using specific language.
+        /// </summary>
+        /// <param name="fileName">The image file name.</param>
+        /// <param name="language">The language.</param>
+        /// <param name="useDefaultDictionaries">Use default dictionaries or not.</param>
+        /// <param name="folder">The image folder.</param>
+        /// <returns><see cref="OCRResponse"/> with the operation result.</returns>
+        public OCRResponse ExtractText(string imageFileName, LanguageName language, bool useDefaultDictionaries = false, string folder = null)
+        {
+            //build URI to extract text
+            string strURI = "";
+            if (string.IsNullOrEmpty(folder))
+                strURI = Product.BaseProductUri + "/ocr/" + imageFileName + "/recognize?language=" + language + "&useDefaultDictionaries=" + useDefaultDictionaries;
+            else
+                strURI = Product.BaseProductUri + "/ocr/" + imageFileName + "/recognize?language=" + language + "&useDefaultDictionaries=" + useDefaultDictionaries + "&folder=" + folder;
+
+            //sign URI
+            string signedURI = Utils.Sign(strURI);
+
+            //execute signed URI request and get response
+            Stream responseStream = Utils.ProcessCommand(signedURI, "GET");
+
+            StreamReader reader = new StreamReader(responseStream);
+            string strJSON = reader.ReadToEnd();
+
+            //Parse the json string to JObject
+            JObject parsedJSON = JObject.Parse(strJSON);
+
+            //Deserializes the JSON to a object. 
+            OCRResponse ocrResponse = JsonConvert.DeserializeObject<OCRResponse>(parsedJSON.ToString());
+
+            return ocrResponse;
+        }
+        /// <summary>
+        /// Extract image text from local file.
+        /// </summary>
+        /// <param name="localFile">The local file.</param>
+        /// <param name="language">The text language.</param>
+        /// <param name="useDefaultDictionaries">Use default dictionaries or not.</param>
+        /// <returns><see cref="OCRResponse"/> with the operation result.</returns>
+        public OCRResponse ExtractText(string localFile, LanguageName language, bool useDefaultDictionaries = false)
+        {
+            //build URI to extract text
+            string strURI = strURI = Product.BaseProductUri + "/ocr/recognize?language=" + language + "&useDefaultDictionaries=" + useDefaultDictionaries;
+
+            //sign URI
+            string signedURI = Utils.Sign(strURI);
+            FileStream stream = new FileStream(localFile, FileMode.Open, FileAccess.Read);
+
+            //execute signed URI request and get response
+            Stream responseStream = Utils.ProcessCommand(signedURI, "POST", stream);
+
+            StreamReader reader = new StreamReader(responseStream);
+            string strJSON = reader.ReadToEnd();
+
+            //Parse the json string to JObject
+            JObject parsedJSON = JObject.Parse(strJSON);
+
+            //Deserializes the JSON to a object. 
+            OCRResponse ocrResponse = JsonConvert.DeserializeObject<OCRResponse>(parsedJSON.ToString());
+
+            return ocrResponse;
+        }
+        /// <summary>
+        /// Extract image text from stream.
+        /// </summary>
+        /// <param name="stream">The stream with image data.</param>
+        /// <param name="language">Language.</param>
+        /// <param name="useDefaultDictionaries">Use default dictionaries or not.</param>
+        /// <returns><see cref="OCRResponse"/> with the operation result.</returns>
+        public OCRResponse ExtractText(Stream stream, LanguageName language, bool useDefaultDictionaries = false)
+        {
+            //build URI to extract text
+            string strURI = strURI = Product.BaseProductUri + "/ocr/recognize?language=" + language + "&useDefaultDictionaries=" + useDefaultDictionaries;
+
+            //sign URI
+            string signedURI = Utils.Sign(strURI);
+
+            //execute signed URI request and get response
+            Stream responseStream = Utils.ProcessCommand(signedURI, "POST", stream);
+
+            StreamReader reader = new StreamReader(responseStream);
+            string strJSON = reader.ReadToEnd();
+
+            //Parse the json string to JObject
+            JObject parsedJSON = JObject.Parse(strJSON);
+
+            //Deserializes the JSON to a object. 
+            OCRResponse ocrResponse = JsonConvert.DeserializeObject<OCRResponse>(parsedJSON.ToString());
+
+            return ocrResponse;
+        }
+
         /// <summary>
         /// Scans whole or part of images and extracts OCR text
         /// </summary>
@@ -64,6 +191,36 @@ namespace Saaspose.OCR
 
             //execute signed URI request and get response
             Stream responseStream = Utils.ProcessCommand(signedURI, "GET");
+
+            StreamReader reader = new StreamReader(responseStream);
+            string strJSON = reader.ReadToEnd();
+
+            //Parse the json string to JObject
+            JObject parsedJSON = JObject.Parse(strJSON);
+
+            //Deserializes the JSON to a object. 
+            OCRResponse ocrResponse = JsonConvert.DeserializeObject<OCRResponse>(parsedJSON.ToString());
+
+            return ocrResponse;
+        }
+
+        /// <summary>
+        /// Extract Text from remote image URL.
+        /// </summary>
+        /// <param name="url">The image url.</param>
+        /// <param name="language">The specific language.</param>
+        /// <param name="useDefaultDictionaries">Use default dictionaries or not.</param>
+        /// <returns><see cref="OCRResponse"/> with the operation result.</returns>
+        public OCRResponse ExtractTextFromURL(string url, LanguageName language, bool useDefaultDictionaries = false)
+        {
+            //build URI to extract text
+            string strURI = Product.BaseProductUri + "/ocr/recognize?url=" + url + "&language=" + language + "&useDefaultDictionaries=" + useDefaultDictionaries;
+
+            //sign URI
+            string signedURI = Utils.Sign(strURI);
+
+            //execute signed URI request and get response
+            Stream responseStream = Utils.ProcessCommand(signedURI, "POST");
 
             StreamReader reader = new StreamReader(responseStream);
             string strJSON = reader.ReadToEnd();
